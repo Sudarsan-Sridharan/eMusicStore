dfa9abfc426ac4e0bbf6a2d9e8e64a6370731c59
Vladimir Bilenko <vbilenko@yahoo.com>
2016-12-01 21:05:49 -0800

    spring web-flow config

--- src/main/java/com/emusicstore/dao/CartDao.java
+++ src/main/java/com/emusicstore/dao/CartDao.java
@@ -2,14 +2,17 @@ package com.emusicstore.dao;
 
 import com.emusicstore.model.Cart;
 
+import java.io.IOException;
+
 /**
  * Created by vbilenko on 11/5/16.
  */
 
 public interface CartDao {
 
-    Cart getCartById(int cartId);
+    Cart getCartById (int cartId);
+
+    Cart validate(int cartId) throws IOException;
 
     void update(Cart cart);
-
 }
--- src/main/java/com/emusicstore/dao/impl/CartDaoImpl.java
+++ src/main/java/com/emusicstore/dao/impl/CartDaoImpl.java
@@ -2,11 +2,14 @@ package com.emusicstore.dao.impl;
 
 import com.emusicstore.dao.CartDao;
 import com.emusicstore.model.Cart;
+import com.emusicstore.service.CustomerOrderService;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.io.IOException;
 
 /**
  * Created by vbilenko on 11/19/16.
@@ -14,22 +17,35 @@ import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 @Transactional
-public class CartDaoImpl implements CartDao {
+public class CartDaoImpl implements CartDao{
 
     @Autowired
     private SessionFactory sessionFactory;
 
-    public Cart getCartById(int cartId) {
+    @Autowired
+    private CustomerOrderService customerOrderService;
+
+    public Cart getCartById (int cartId) {
         Session session = sessionFactory.getCurrentSession();
         return (Cart) session.get(Cart.class, cartId);
     }
 
-    public void updtate(Cart cart) {
-
-    }
-
     public void update(Cart cart) {
         int cartId = cart.getCartId();
-        //TODO: do later
+        double grandTotal = customerOrderService.getCustomerOrderGrandTotal(cartId);
+        cart.setGrandTotal(grandTotal);
+
+        Session session = sessionFactory.getCurrentSession();
+        session.saveOrUpdate(cart);
+    }
+
+    public Cart validate(int cartId) throws IOException {
+        Cart cart=getCartById(cartId);
+        if(cart==null||cart.getCartItems().size()==0) {
+            throw new IOException(cartId+"");
+        }
+        update(cart);
+        return cart;
     }
 }
+
